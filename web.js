e8824cc2c10e78bc60fb66f9dc0f4ed1c891a793
Phil Rabin <phil@prismskylabs.com>
2013-09-22 20:35:09 -0700

    move to node micro app for heroku

---
+++ web.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.logger());
+app.use(express.static(__dirname + '/static'));
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.set('title', 'SF Food Truck Locations');
+
+app.get('/', function(request, response) {
+    response.render('index.html');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
